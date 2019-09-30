72e081ea2cdb40a5ded059060b3c8fc2e7e37c9b
Aidan Crispin <acrispin5466@gmial.com>
2019-09-30 09:21:28 -0500

    Debugging

--- src/debugging/FixDebugSix3.java
+++ src/debugging/FixDebugSix3.java
@@ -5,29 +5,24 @@
 // With a brief pause between each displayed value
 package debugging;
 import javax.swing.*;
-public class FixDebugSix3
-{
-    public static void main(String[] args)
-    {
+public class FixDebugSix3 {
+    public static void main(String[] args) {
         String userNumString;
-        int userNum val;
+        int userNum, val;
         final int MIN = 1;
         final int MAX = 20;
         userNumString = JOptionPane.showInputDialog(null,
                 "Enter a number between " + MIN + " and " + MAX + " inclusive");
         userNum = Integer.parseInt(userNumString);
-        while(userNum < MIN | userNum > MAX)
-        {
-            //userNumString = JOptionPane.showInputDialog(null,
-            "Number out of range" +
-                    "\nEnter a number between " + MIN + " and " + MAX + " inclusive");
+        while(userNum < MIN || userNum > MAX) {
+            userNumString = JOptionPane.showInputDialog(null,
+            "Number out of range" + "\nEnter a number between " + MIN + " and " + MAX + " inclusive");
             userNum = Integer.parseInt(userNumString);
         }
-        for(val  userNum; val  0; -val)
-        {
+        for(val = userNum; val != 0; --val){
             System.out.print(val + "  ");
-            for(int x = 0; x  100000; ++x)
-            for(int y = 0y  10000; ++y);
+            for(int x = 0; x < 1000000000; ++x)
+            for(int y = 0; y < 1000000000; ++y);
             // Adjust these numbers for faster or slower performance
         }
         System.out.println("Blastoff!");
